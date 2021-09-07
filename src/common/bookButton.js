b87495901f06e108a1034c1390459d0432b59402
Hiteshshingala <shingalahitesh2@gmail.com>
2021-09-07 23:33:46 +0530

    - fix minor issue

--- src/common/bookButton.js
+++ src/common/bookButton.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router';
-import {Button, DropdownButton, Dropdown} from 'react-bootstrap'
+import { Button, DropdownButton, Dropdown } from 'react-bootstrap'
 import { Container, Row, Col, Modal } from "react-bootstrap";
 import { getData, setData } from '../services/localstorage'
 import { DateRangePicker } from 'react-date-range';
+import addDays from 'date-fns/addDays';
 
 const BookButton = ({ values, setUserData }) => {
     let history = useHistory();
     const [resourceData, setResourceData] = useState([]);
+    const [selectedResourceData, setSelectedResourceData] = useState('Select Product');
     const [bookingDate, setBookingDate] = useState({
         startDate: new Date(),
         endDate: new Date(),
@@ -20,59 +22,69 @@ const BookButton = ({ values, setUserData }) => {
     }
 
     useEffect(() => {
-      getBookingData();
+        getBookingData();
     }, []);
-  
+
     const getBookingData = () => {
-      const data = getData();
-      if(data && data.length > 0) {
-          setResourceData(data);
-      }
+        const data = getData();
+        if (data && data.length > 0) {
+            setResourceData(data);
+        }
     };
 
     const closeModal = () => {
         setIsModalOpen(false);
-      };
+    };
 
-    const handleSelect = ({selection}) => {
-        const {startDate, endDate} = selection;
-        setBookingDate({
-            startDate: startDate,
-            endDate: endDate,
-            key: 'selection',
-        })
+    const handleSelect = ({ selection }) => {
+        if (selection) {
+            const { startDate, endDate } = selection;
+            setBookingDate({
+                startDate: startDate,
+                endDate: endDate,
+                key: 'selection',
+            })
+        }
+    }
+
+    const selectResource = (e) => {
+        setSelectedResourceData(e);
     }
     const dayContentRenderer = (selection) => {
-        console.log('@@@dayContentRenderer', selection)
     }
 
-      
+
     return (
         <>
-        <div>
-             <Button variant="primary" onClick={() => handleClick()}>Book</Button>
-        </div>
-              <Modal
+            <div>
+                <Button variant="primary" onClick={() => handleClick()}>Book</Button>
+            </div>
+            <Modal
                 className="booking-modal"
                 show={isModalOpen}
                 backdrop={true}
                 onHide={closeModal}
-                >
-                    <div>
-                        <DropdownButton align="end" title="Dropdown end" id="dropdown-menu-align-end">
-                            {resourceData.map(el => {
-                                return (
-                                    <Dropdown.Item eventKey="1">{el.name} / {el.code}</Dropdown.Item>
-                                )
-                            })}
-                        </DropdownButton>
-                        <DateRangePicker
-                            ranges={[bookingDate]}
-                            onChange={handleSelect}
-                            dayContentRenderer={dayContentRenderer}
-                            disabledDates={new Date()}
-                        />
-                    </div>
+            >
+                <div>
+                    <DropdownButton align="end" onSelect={selectResource} title={selectedResourceData} id="dropdown-menu-align-end">
+                        {resourceData.map(el => {
+                            return (
+                                <Dropdown.Item eventKey={`${el.name}/${el.code}`}> {el.name} / {el.code}</Dropdown.Item>
+                            )
+                        })}
+                    </DropdownButton>
+                    <DateRangePicker
+                        ranges={[bookingDate]}
+                        onChange={handleSelect}
+                        dayContentRenderer={dayContentRenderer}
+                        disabledDates={[
+                            addDays(new Date(), 1),
+                            addDays(new Date(), 2),
+                            addDays(new Date(), 3),
+                        ]}
+                    >
+                    </DateRangePicker>
+                </div>
             </Modal>
         </>
     )
